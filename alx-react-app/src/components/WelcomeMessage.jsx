a1ae26773f35c9e7ade8c37a376bf4e45d2e7c9c
choene_mc <willchoenemc@gmail.com>
2025-08-28 22:58:46 +0200

    Update components with latest changes

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone i am learning via ALX!!</h1>
+            <p>this is a simple JSX component</p>
+            <p>i am learning about JSX</p>
+        </div>
+    )
+};
