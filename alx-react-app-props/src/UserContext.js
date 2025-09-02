693b9f379cc4ee9d670d23dcd4f3d0370bda1750
choene_mc <willchoenemc@gmail.com>
2025-09-02 15:00:30 +0200

    Update UserContext.js with latest changes

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,14 @@
+
+import React, { createContext } from 'react';
+
+
+export const UserContext = createContext();
+
+
+export const UserProvider = ({ children, value }) => {
+  return (
+    <UserContext.Provider value={value}>
+      {children}
+    </UserContext.Provider>
+  );
+};
