a0da5439e0d031e664e3243bc89a39ed19de7eb1
choene_mc <willchoenemc@gmail.com>
2025-09-02 12:47:07 +0200

    Update App.jsx with latest changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
