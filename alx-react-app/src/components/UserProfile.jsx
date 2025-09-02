ac2d0de9e9c38926c097e94ec1a20cabfb35814b
choene_mc <willchoenemc@gmail.com>
2025-09-02 10:17:07 +0200

    Update UserProfile.jsx with latest changes

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,9 +3,9 @@ import UserProfile from "./components/UserProfile";
 function App() {
   return (
     <div>
-      <h1>Nare Mojela</h1>
-      <p>Age: 30</p>
-      <p>Bio: Love Listening to music and playing soccer</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
       </div>
        
   );
