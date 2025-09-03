3bb4f4db278e1e5b8a002d845c133fe51c29c787
choene_mc <willchoenemc@gmail.com>
2025-09-03 20:54:24 +0200

    Update UserContext.js with latest  changes

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,15 @@
 
-import React, { createContext } from "react";
+
+import React, { createContext, useContext } from 'react';
 
 const UserContext = createContext();
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+};
 
 export default UserContext;
