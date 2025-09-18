5225b539c9015a8581f1fd5841edb5f0520cfa19
choene_mc <willchoenemc@gmail.com>
2025-09-18 14:13:16 +0200

    Update githubService.js with lates changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,7 +6,7 @@ const BASE_URL = "https://api.github.com/users";
 @param {string} username - Github username
 @returns {promise<object>} -User data
 */
-export const fetchGithubUser = async (username) => {
+export const fetchUserData = async (username) => {
     try {   
         const response = await axios.get(`${BASE_URL}/${username}`);
         return response.data;
