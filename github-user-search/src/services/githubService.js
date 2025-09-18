ffb45f4a08b0d83436b9d5f15a3b500e09e3f046
choene_mc <willchoenemc@gmail.com>
2025-09-18 14:31:05 +0200

    Update githubService.js with latest changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users";
+export const fetchUsers = async ({ username, location, minRepos }) => {
+  let query = username ? `${username} in:login` : "";
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
 
-/** 
-@param {string} username - Github username
-@returns {promise<object>} -User data
-*/
-export const fetchUserData = async (username) => {
-    try {   
-        const response = await axios.get(`${BASE_URL}/${username}`);
-        return response.data;
-    } catch (error) {
-        throw error; 
-    }
-}
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  // GitHub API returns results under response.data.items
+  const users = response.data.items;
+
+  // Fetch additional info for each user
+  const detailedUsers = await Promise.all(
+    users.map(async (user) => {
+      const details = await axios.get(user.url);
+      return details.data;
+    })
+  );
+
+  return detailedUsers;
+};
