9055a7b91b071f052aabf91e83acf183990bd7ae
choene_mc <willchoenemc@gmail.com>
2025-09-20 01:39:33 +0200

    Update github-user-search with latest changes

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {export('taildwindcss').config} */
+export default {
+    content: [ ".index.html", "./src/**/*.{js,jsx,ts,tsx} "], 
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
