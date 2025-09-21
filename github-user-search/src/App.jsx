4fefa752b31af81b37a90dacdf72845082b6bdc3
choene_mc <willchoenemc@gmail.com>
2025-09-21 11:00:48 +0200

    Update github-user-search with latest changes

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,18 @@
-import search from "./components/search";
+import Search from "./components/Search";
 
 function App() {
-  return (
+return (
+  <>
     <div style={{ padding: "20px"}}>
       <h1>Github Users search</h1>
-      <search />
+      <Search />
     </div>
-  )
+    <div className="bg-blue-500 text-white p-4 rounded">
+      🚀 Tailwind is working!
+    </div>
+  </>
+)
+
 }
 
 export default App;
--- github-user-search/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {export('taildwindcss').config} */
-export default {
-    content: [ ".index.html", "./src/**/*.{js,jsx,ts,tsx} "], 
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
